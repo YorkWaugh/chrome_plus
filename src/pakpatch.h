57581a5bd76808adc33baa57e67cdee5ccef7421
Bush2021 <79072750+Bush2021@users.noreply.github.com>
2024-07-18 00:26:13 -0400

    Build 37ede4d

--- src/pakpatch.h
+++ src/pakpatch.h
@@ -58,7 +58,7 @@ HANDLE WINAPI MyMapViewOfFile(_In_ HANDLE hFileMappingObject,
                 R"(hidden="true")");
           }
 
-          const char prouct_title[] = u8R"({aboutBrowserVersion}</div><div class="secondary"><a target="_blank" href="https://github.com/Bush2021/chrome_plus">Chrome++</a> )" RELEASE_VER_STR u8R"( modified version</div>)";
+          const char prouct_title[] = u8R"({aboutBrowserVersion}</div><div class="secondary"><a target="_blank" href="https://github.com/Bush2021/chrome_plus">Chrome++</a> )" "alpha-37ede4d" u8R"( modified version</div>)";
           ReplaceStringInPlace(html, R"({aboutBrowserVersion}</div>)",
                                prouct_title);
 
